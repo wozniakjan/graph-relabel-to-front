c88933b05e17c2f22ee7570fd4e7117509a5d827
Jan Wozniak <xwozni00@stud.fit.vutbr.cz>
2012-12-08 14:15:16 +0100

    GraphListener comments

--- relabel-to-front/src/Graph/GraphListener.java
+++ relabel-to-front/src/Graph/GraphListener.java
@@ -9,9 +9,18 @@ package Graph;
  * @author Jan
  */
 public interface GraphListener {
+    //Waits until user requests another step
     public void yield();
+    
+    //Changes value of flow from Node U to Node V
     public void change_flow(int u, int v, int value);
+    
+    //Changes height of Node U
     public void change_height(int u, int value);
+    
+    //Prints lovely message for user
     public void print_message(String message);
+    
+    //Prints result value of total maximal flow in the graph
     public void print_total_flow(int flow);
 }
